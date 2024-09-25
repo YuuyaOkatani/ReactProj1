d285ab44e4de15182dd44c6947cbef7750983f5f
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-09-24 21:15:19 -0300

    refactor

--- App.js
+++ App.js
@@ -1,20 +1,214 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// JSX
+// import { Component } from "react";
+// import { StatusBar } from 'expo-status-bar';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { collection, doc, setDoc, addDoc, updateDoc, deleteDoc, getDoc, getDocs } from "firebase/firestore";
+// import {db} from './config';
+
+import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList, ScrollView, TouchableOpacity, Image} from 'react-native';
+import React, { useState } from 'react';
+
+
+
+
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
+import { initializeApp} from 'firebase/app';
+
+
+
+import { firebaseConfig } from './api/firebase-config.js';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useSharedValue } from 'react-native-reanimated';
+
+
+
+function HomeScreen({navigation}){
+	return (
+    <Text style={{fontSize:17, fontWeight:'400', color:'blue'}}>Home</Text>)
+	
+}
+
+const height = useSharedValue(0);
+  const activateAnimation = () => {
+    height.value += 100;
+  }
+
+function Login({navigation}){
+  
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  
+  const app=initializeApp(firebaseConfig); // Crio a conexão com o firebase
+  
+  const auth=getAuth(app); // Acesso o recurso
+  
+  
+  
+  const handleCreateAccount=()=>{
+      createUserWithEmailAndPassword(auth,email,password)
+      .then((userCredential)=>
+      {
+        console.log ('Conta Criada!');
+        const user=userCredential.user;
+        console.log(user)
+        Alert.alert('Cadastrado com Sucesso!')
+      })
+      .catch(error=>{
+        console.log(error)
+        Alert.alert(error.message)
+      })
+  }
+  
+  const handleSignIn=()=>{
+    signInWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=>
+      {
+        console.log ('Usuário Logado!');
+        const user=userCredential.user;
+        console.log (user)
+        Alert.alert('LogIn efetuado com sucesso!');
+        navigation.navigate('Home');
+      })
+      .catch(error=>{
+        console.log(error)
+        Alert.alert(error.message)
+      })
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {height}]}>
+      
+      <ScrollView contentContainerStyle={{
+        flex:1,
+        width:'100%',
+        height:'100%',
+        alignItems:'center',
+        justifyContent:'center',
+      }}>
+        
+          <View style={styles.login}>
+            
+            <View>
+
+              <TextInput style={styles.input} placeholder="E-Mail" onChangeText={(text)=>setEmail(text)}/>
+            </View>
+          <View>
+          
+            <TextInput style={styles.input} placeholder="Password" onChangeText={(text)=>setPassword(text)} secureTextEntry={true}/>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+            <Text style={{fontSize:17, fontWeight:'400', color:'white'}}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
+            <Text style={{fontSize:17, fontWeight:'400', color:'white'}}>Criar Conta</Text>
+          </TouchableOpacity>
+          </View>
+       
+      </ScrollView>
     </View>
   );
 }
 
+//const Stack=createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App(){
+
+  
+	activateAnimation(); 
+return (
+//    <NavigationContainer>
+//        <Stack.Navigator initialRouteName="Login">
+//          <Stack.Screen name="Login" component={Login} />
+//          <Stack.Screen name="Home" component={HomeScreen} />
+//          
+//        </Stack.Navigator>
+//    </NavigationContainer>
+
+<NavigationContainer>
+      <Drawer.Navigator initialRouteName="Login">
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
+</NavigationContainer>
+
+
+);
+}
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#02183b',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width:'100%',
+    height:'100%',
+    resizeMode:'cover',
+  },
+  login:{
+    width:350,
+    height:500,
+    borderColor:'#fff',
+    borderWidth:2,
+    borderRadius:10,
+    padding:10,
+    alignItems:'center',
+  },
+  profilePicture:
+  {
+     width:100,
+     height:100,
+     borderRadius:50,
+     borderColor:'#fff',
+     borderWidth:1,
+     marginVertical:30,
+  },
+  input: {
+    width:250,
+    height:40,
+    borderColor:'#4d8aeb',
+    borderWidth:3, 
+    padding:10,
+    marginVertival:10,
+    backgroundColor:'#ffffff',
+    marginBottom: 20,
+  },
+    button:{
+      width:250,
+      height:40,
+      bordercolor:10,
+      backgroundColor:'#00000090',
+      alignItems:'center',
+      justifyContent:'center',
+      marginVertical:10,
+      borderColor:'#fff',
+      borderWidth:2,
+    },
+    blurContainer: {
+      flex: 1,
+      padding: 20,
+      justifyContent: 'center',
+    },
+textBoxes:
+  {
+    width:'90%',
+    fontSize:18,
+    padding:12,
+    borderColor:'gray',
+    borderWidth:0.2,
+    borderRadius:10
+  },
 });
+
+//export default App;
+
---
+++ api/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyC9keYCzYRw6QVZtk4LJCFRMZ17uOEggqc",
+  authDomain: "ihcproject-5a388.firebaseapp.com",
+  projectId: "ihcproject-5a388",
+  storageBucket: "ihcproject-5a388.appspot.com",
+  messagingSenderId: "103491525485",
+  appId: "1:103491525485:web:1687d196acf3d78d7e1864"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
